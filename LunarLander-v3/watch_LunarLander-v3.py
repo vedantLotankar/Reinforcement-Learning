58f6687aba594748237df46fc4742042943a1684
vedantLotankar <lotankarvedant10@gmail.com>
2025-02-06 20:08:26 +0530

    Add requirements and watch script for LunarLander-v3 environment

---
+++ LunarLander-v3/watch_LunarLander-v3.py
@@ -0,0 +1,20 @@
+import gymnasium as gym
+from stable_baselines3 import DQN
+
+model = DQN.load("Reinforcement-Learning/LunarLander-v3/dqn_lunarlander")
+
+env = gym.make("LunarLander-v3", render_mode = "human")
+
+# Reset the environment to get the initial observation
+obs, _ = env.reset()
+
+# Run the simulation
+for _ in range(500):  # 500 steps (adjust if you want to watch longer!)
+    action, _ = model.predict(obs, deterministic=True)  # Get action from model
+    obs, reward, done, _, _ = env.step(action)  # Apply action to the environment
+    env.render()  # Visualize the environment
+    
+    if done:
+        obs, _ = env.reset()  # Reset environment if done (just in case)
+
+env.close()  # Close the environment when done
